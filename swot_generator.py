d8d04d71165afca0f4b04dd68fc0f77b2e084796
KV-Strat <kirthiv123@gmail.com>
2025-07-25 02:45:12 -0700

    Update swot_generator.py

--- swot_generator.py
+++ swot_generator.py
@@ -6,7 +6,7 @@ from openai import OpenAI
 
 load_dotenv()
 openai.api_key = st.secrets["OPENAI_API_KEY"]
-#client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 
 def generate_strategy(company, competitors, industry):
